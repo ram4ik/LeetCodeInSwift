19943b34144e4134b0b4351de5cfd229e7d19351
ram4ik <ram4ik@hotmail.com>
2024-01-08 21:21:42 +0200

    Initial Commit

---
+++ LeetCodeInSwift/LeetCodeInSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetCodeInSwiftApp.swift
+//  LeetCodeInSwift
+//
+//  Created by Ramill Ibragimov on 08.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct LeetCodeInSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
