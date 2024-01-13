08ed97ba1ddbc165b119337464ed58478f8a24ca
ram4ik <ram4ik@hotmail.com>
2024-01-13 10:57:21 +0200

    single number

---
+++ LeetCodeInSwift/SingleNumber.swift
@@ -0,0 +1,30 @@
+//
+//  SingleNumber.swift
+//  LeetCodeInSwift
+//
+//  Created by Ramill Ibragimov on 13.01.2024.
+//
+
+import Foundation
+
+class SingleNumber {
+    
+    func singleNumber(_ nums: [Int]) -> Int {
+        var m = [Int: Int]()
+        for i in 0..<nums.count {
+            let num = nums[i]
+            if let count = m[num] {
+                m[num] = count + 1
+            } else {
+                m[num] = 1
+            }
+        }
+        for i in 0..<nums.count {
+            let num = nums[i]
+            if m[num] == 1 {
+                return num
+            }
+        }
+        return -1
+    }
+}
---
+++ LeetCodeInSwiftTests/SingleNumberTest.swift
@@ -0,0 +1,39 @@
+//
+//  SingleNumberTest.swift
+//  LeetCodeInSwiftTests
+//
+//  Created by Ramill Ibragimov on 13.01.2024.
+//
+
+import XCTest
+@testable import LeetCodeInSwift
+
+final class SingleNumberTest: XCTestCase {
+
+    let sn = SingleNumber()
+    
+    func testExample1() throws {
+        
+        XCTAssertEqual(sn.singleNumber([2, 2, 1]), 1)
+    }
+    
+    func testExample2() throws {
+        
+        XCTAssertEqual(sn.singleNumber([2, 2, 1, 1]), -1)
+    }
+    
+    func testExample3() throws {
+        
+        XCTAssertEqual(sn.singleNumber([4, 1, 2, 1, 2]), 4)
+    }
+    
+    func testExample4() throws {
+        
+        XCTAssertEqual(sn.singleNumber([1, 1, 4]), 4)
+    }
+    
+    func testExample5() throws {
+        
+        XCTAssertEqual(sn.singleNumber([1, 1, 2, 2, 5, 5, 9]), 9)
+    }
+}
