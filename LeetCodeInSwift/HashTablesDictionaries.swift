9828686433f39d8d602bced106726936d9e5171a
ram4ik <ram4ik@hotmail.com>
2024-01-18 14:04:05 +0200

    Hash Tables Dictionaries

--- LeetCodeInSwift/HashTablesDictionaries.swift
+++ LeetCodeInSwift/HashTablesDictionaries.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+class HashTablesDictionaries {
+    
+    func fourSumCount(_ A: [Int], _ B: [Int], _ C: [Int], _ D: [Int]) -> Int {
+        var m: [Int: Int] = [:]
+        
+        for a in A {
+            for c in C {
+                let target = a + c
+                if m[target] == nil {
+                    m[target] = 0
+                }
+                m[target]! += 1
+            }
+        }
+        
+        var ans = 0
+        for b in B {
+            for d in D {
+                let target = -(b + d)
+                if m[target] != nil {
+                    ans += m[target]!
+                }
+            }
+        }
+        return ans
+    }
+}
